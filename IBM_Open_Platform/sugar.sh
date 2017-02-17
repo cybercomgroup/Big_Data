962f1dd7c6c76cced3875ed9541426e68607ded7
phillebaba <philip.laine@gmail.com>
2017-02-17 10:14:16 -0500

    Added helper file to run MapReduce and Weka jobs.

---
+++ IBM_Open_Platform/sugar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Parameters:
+# 	MapReduce jar in Local
+#	Input path in HDFS
+#	Output path in HDFS
+#	Weka jar in Local (Optional)
+
+if [ $# -lt 3 ]; then
+	echo "Missing required arguments [MapReduce] [Input] [Output]"
+	exit
+fi
+
+if [[ ! -f $1 ]]; then
+	echo "MapReduce jar does not exist"
+	exit
+fi
+
+if [ hadoop fs -test -d $2 != 0 ]; then
+	echo "Input directory does not exist
+	exit
+fi
+
+now = date + "%m-%d-%y_%H:%M:%S"
+outputPath = "$3/$now"
+
+hadoop jar $1 $2 $outputPath
+
+if [ $# -ne 4 ]; then
+	exit
+fi
+
+hadoop jar $4 "$outputPath/part-m-00000"
