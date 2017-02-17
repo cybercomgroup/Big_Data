433fa6cef3d14181e7c4cab5f1a80f2ca1dbac2b
Erenstedt <erenstedt@gmail.com>
2017-02-17 16:06:18 +0100

    Finished the mapreduce job, now parses 'perfectly' into a csv file.

--- IBM_Open_Platform/JavaCode/TitanicMapReduce/src/Titanic.java
+++ IBM_Open_Platform/JavaCode/TitanicMapReduce/src/Titanic.java
@@ -84,26 +84,19 @@ public class Titanic extends Configured implements Tool {
 
 
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import au.com.bytecode.opencsv.CSVReader;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.aggregate.DoubleValueSum;
 import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.*;
 import org.apache.hadoop.util.*;
-import weka.classifiers.evaluation.output.prediction.CSV;
 
 public class Titanic extends Configured implements Tool {
 
@@ -118,88 +111,107 @@ public class Titanic extends Configured implements Tool {
 
         Configuration conf = getConf();
 
-        Job job = Job.getInstance(conf, this.getClass().toString());
+        //Found all these on different versions on StackOverflow to separate the key and value with something else than "\t"
+            //  String separator = ",";
+            //  conf.set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", separator); // adds ", between key and value"
+            //  conf.set("key.value.separator.in.input.line", separator);
+            //  conf.set("mapred.textoutputformat.separator", separator); //Prior to Hadoop 2 (YARN)
+            //  conf.set("mapreduce.textoutputformat.separator", separator);  //Hadoop v2+ (YARN)
+            //  conf.set("mapreduce.output.key.field.separator", separator);
+            //  conf.set("mapred.textoutputformat.separatorText", separator);
 
+        conf.set("mapreduce.output.textoutputformat.separator", ",");
+
+
+        Job job = Job.getInstance(conf);
+        job.setJobName(this.getClass().toString());
         FileInputFormat.setInputPaths(job, inputPath);
         FileOutputFormat.setOutputPath(job, outputPath);
 
-        job.setJobName("Titanic");
         job.setJarByClass(Titanic.class);
+
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
+
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
+
+        //job.setOutputKeyClass(Text.class);
+        //job.setOutputValueClass(Text.class);
 
         job.setMapperClass(Map.class);
-        //job.setCombinerClass(Reduce.class);
         job.setNumReduceTasks(0);
-        //job.setReducerClass(Reduce.class);
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+        //private final static LongWritable one = new LongWritable();
+        private Text outputKey = new Text();
+        private Text outputData = new Text();
 
         @Override
         public void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
-            // Skip the first header
-            if (Integer.parseInt(key.toString()) == 0) {
-                return;
-            }
-
             CSVReader reader = new CSVReader(new StringReader(value.toString()), ',');
             List<String> csv = Arrays.asList(reader.readNext());
 
-            /*
-            0   PassengerId
-            1   Survived
-            2   Pclass
-            3   Name - getTitle()
-            4   Sex
-            5   Age  - group by 0-18 + 60-* and the rest (Exposed and not exposed)
-            6   SibSp
-            7   Parch
-            8   Ticket - Only the letters
-            9   Fare
-            10  Cabin - Map letter order as value
-            11  Embarked
-            */
 
-            // Replace name with only title
-            csv.set(3, getTitle(csv.get(3)));
+            // Get attributes, should be the .arff format in the future
+            if (Integer.parseInt(key.toString()) == 0) { //This is the header
+                outputKey.set(csv.get(0));
+                outputData.set(String.join(",", csv.subList(1, csv.size())));
+            }else{ //Data
+                
+                /*
+                0   PassengerId //This will be the outputKey
+                1   Survived
+                2   Pclass
+                3   Name - getTitle()
+                4   Sex
+                5   Age  - group by 0-18 + 60-* and the rest (Exposed and not exposed)
+                6   SibSp
+                7   Parch
+                8   Ticket - Only the letters
+                9   Fare
+                10  Cabin - Map letter order as value
+                11  Embarked
+                */
 
-            //Group ages
-            if(csv.get(5).length() > 0) {
-                Double age = Double.parseDouble(csv.get(5));
-                if(age > 18 && age < 60) {
-                    csv.set(5, "1");
-                } else {
-                    csv.set(5, "0");
+                // Replace name with only title
+                csv.set(3, getTitle(csv.get(3)));
+
+                //Group ages
+                if(csv.get(5).length() > 0) {
+                    Double age = Double.parseDouble(csv.get(5));
+                    if(age > 18 && age < 60) {
+                        csv.set(5, "1");
+                    } else {
+                        csv.set(5, "0");
+                    }
+                } else{
+                    csv.set(5, "-1");
                 }
-            } else{
-                csv.set(5, "-1");
+
+
+                // Removes the numbers from the ticket
+                csv.set(8, csv.get(8).replaceAll("[0-9 ]*", "")); //Toxic as shits, replaces in the wrong places
+
+
+                // Ranks the cabin based on letter order or 0
+                if (csv.get(10).isEmpty() == false) {
+                    csv.set(10, (String.valueOf(csv.get(10).charAt(0) - 'A' + 1)));
+
+                } else {
+                    csv.set(10, "0");
+                }
+
+                // Write the result
+                outputKey.set(csv.get(0));
+                outputData.set(String.join(",", csv.subList(1, csv.size())));
+
             }
 
-
-            // Removes the numbers from the ticket
-            csv.set(8, csv.get(8).replaceAll("[0-9 ]*", "")); //Toxic as shits, replaces in the wrong places
-
-
-            // Ranks the cabin based on letter order or 0
-            if (csv.get(10).isEmpty() == false) {
-                csv.set(10, (String.valueOf(csv.get(10).charAt(0) - 'A' + 1)));
-
-            } else {
-                csv.set(10, "0");
-            }
-
-            // Write the result
-            word.set(String.join(",", csv));
-            context.write(word, one);
+            context.write(outputKey, outputData);
         }
     }
 
