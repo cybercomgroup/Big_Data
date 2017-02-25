f1c7eccfd176fb4ced4964c9de8f640926aacfcf
William Björklund <wilbjo@student.chalmers.se>
2017-02-25 14:48:42 +0100

    MORE MORE MORE formatting in order to get csv right

--- Cloudera/Code/million_song_dataset/replace_nan_in_csv.py
+++ Cloudera/Code/million_song_dataset/replace_nan_in_csv.py
@@ -2,10 +2,17 @@ import os
 
 source = open('SongCSV.csv', 'r')
 target = open('SongCSV_fixed.csv', 'w')
+first = True
 
 for row in source:
-	row = row.replace(",nan,", ",,")
-	row = row.replace(",nan", ",")
-	target.write(row)
+    row = row.replace(",nan,", ",,")
+    row = row.replace(",nan", ",")
+    row = row.replace("b'", "")
+    row = row.replace("'", "")
+    if first:
+        first = False
+        target.write(row + "\n")
+    else:
+        target.write(row)
 
 
