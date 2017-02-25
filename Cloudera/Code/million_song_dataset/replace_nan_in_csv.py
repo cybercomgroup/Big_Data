230e55cd813c5b735f901053151f58d51fa308b8
William Björklund <wilbjo@student.chalmers.se>
2017-02-25 14:31:50 +0100

    Deleted extra newline.

--- Cloudera/Code/million_song_dataset/replace_nan_in_csv.py
+++ Cloudera/Code/million_song_dataset/replace_nan_in_csv.py
@@ -1,5 +1,4 @@
 import os
-newline = os.linesep
 
 source = open('SongCSV.csv', 'r')
 target = open('SongCSV_fixed.csv', 'w')
@@ -7,6 +6,6 @@ target = open('SongCSV_fixed.csv', 'w')
 for row in source:
 	row = row.replace(",nan,", ",,")
 	row = row.replace(",nan", ",")
-	target.write(row + newline)
+	target.write(row)
 
 
