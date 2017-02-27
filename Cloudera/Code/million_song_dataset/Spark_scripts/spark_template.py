cd25c9cfa1627786dfa3e60be46b3b962a4850d3
Victor Dahlberg <v.dahlberg01@gmail.com>
2017-02-27 14:03:14 -0800

    Added template spark script

---
+++ Cloudera/Code/million_song_dataset/Spark_scripts/spark_template.py
@@ -0,0 +1,46 @@
+from pyspark import SparkConf
+from pyspark import SparkContext
+import pandas as pd
+
+# To run the spark job, run this command:
+# PYSPARK_PYTHON=/opt/cloudera/parcels/Anaconda/bin/python spark-submit <file>.py 
+
+
+
+# Params: Spark RDD object from a csv-file.
+# Returns: Pandas DF of RDD object
+def rddToPand(RDD):
+
+	header = "temp"
+	first = True
+	data = []
+
+	# Convert unicode to ascii
+	for x in RDD.collect():
+		if first:
+			first = False
+			header = x.encode("ascii").split(',')
+
+		else:
+			data.append(tuple(x.encode("ascii").split(',')))
+
+	return pd.DataFrame.from_records(data, columns = header)
+
+# Init Spark
+conf = SparkConf()
+conf.setMaster('yarn-client')
+conf.setAppName('template-job')
+sc = SparkContext(conf=conf)
+
+# Read some csv from hdfs
+rdd = sc.textFile("hdfs:///user/cloudera/song/test.csv")
+
+# Parse rdd to pandas object
+df = rddToPand(rdd)
+
+# Do what you want with the pandas dataframe
+file = open('output.txt', 'w')
+file.write(str(df.describe()))
+file.write("\n\n-------------------------\n\n")
+file.write(str(df.head()))
+
